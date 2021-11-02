672267cb07f8bb3f14fd2c2b7bbc34b531d2e6ee
rachana <bolla.rachana@gmail.com>
2021-11-02 03:04:07 -0700

    url

--- hw3/02-url.js
+++ hw3/02-url.js
@@ -1,6 +1,35 @@
-// Enter your code here
 
-console.log('Enter your code here');
+function getParameters() {
+  let urlstr1 = document.getElementById("comments").value;
+
+   let urlString = urlstr1.replace('%40','@')
+  
+  
+               let paramString = urlString.split('?')[1];
+               
+               var urlst = "";
+      if(paramString==null)
+      {
+       urlst = urlString.split('?')[0];
+    }
+    else{
+      
+        urlst = urlString.split('?')[0] +"<br><br>" + "Query Parameters";
+       
+    
+       
+               let params_arr = paramString.split('&');
+               
+               for (let i = 0; i < params_arr.length; i++) {
+                     
+                   let pair = params_arr[i].split('=');
+                   urlst = urlst + "<br>" + pair[0] + " : " +pair[1];
+                   console.log("Key is:" + pair[0]);
+                   console.log("Value is:" + pair[1]);
+               }
+            } 
+               document.getElementById("results").innerHTML = urlst;
+} 
 
 // Test Input #1:
 // http://www.example.com
