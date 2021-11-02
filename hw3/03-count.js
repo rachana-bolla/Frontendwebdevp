d0d47f56be9fabe7f31ce847948be606293acd33
rachana <bolla.rachana@gmail.com>
2021-11-02 14:49:02 -0700

    count

--- hw3/03-count.js
+++ hw3/03-count.js
@@ -1,3 +1,19 @@
-// Enter your code here
+function search()
+{
+    const p = document.getElementById("p");    
+    
+    const special =/[\\[{().+*?|^$]/g;;
+    let input=document.getElementById("input").value;
 
-console.log('Enter your code here');
+    if(input!=="")
+    {
+      /* below line if we do not add the + " " then it will show, letter which happen to be in middle of words  */
+         input = " " + input + " ";
+        if(special.test(input)) input = input.replace(special,"\\$&");
+        let regExp = new RegExp(input,"gi");
+      // window.alert(p.textContent);
+        p.innerHTML = (p.textContent).replace(regExp, "<mark>$&</mark>");
+        
+    }
+
+}
