68bc4f17da2a14bf98ab570be0c7d9fa39971eda
rachana <bolla.rachana@gmail.com>
2021-11-03 00:05:37 -0700

    background

--- hw3/05-background.js
+++ hw3/05-background.js
@@ -1,65 +1,50 @@
-// Enter your code here
-var button;
-function counttext()
-{
-    
-   
-    button = document.getElementById("Start").value;
-    document.getElementById("Start").disabled = true;
-    document.getElementById("Start").hidden = true;
-    document.getElementById("Stop").disabled = false;
-    document.getElementById("Stop").hidden = false;
+function init(){
 
-    
 
-var index = Math.round(Math.random() * 9);
-var ColorValue = "FFFFFF"; // default color - white (index = 0)
-
-var inputVal = document.getElementById("Start").value;
-
-var r = Math.floor(Math.random() * 256);
-var g = Math.round(Math.random() * 256);
-var b = Math.round(Math.random() * 256);
-var b = Math.round(Math.random() * 256);
-
-ColorValue = "rgba("+r+","+g+","+b+",0.1)";
- //  window.alert(ColorValue);
-                     
-    document.getElementsByTagName("body")[0].style.backgroundColor = ColorValue;
   
- //counttext();
-// window.alert(document.getElementById("interval").value*1000); // background color change on every 5 sec
+  //  imgObj = document.getElementById('myImage');
+    //imgObj.style.position= 'relative';
+   // imgObj.style.left = '0px';
+    }
     
- if(document.getElementById("Start").hidden == true)
- {
- window.setTimeout( "counttext()" , document.getElementById("interval").value*1000); // background color change on every 5 sec
- }
- else{
+    function startStop(){
+        document.getElementById("startButton").hidden = true;
+        document.getElementById("stopButton").hidden=false;
+    moveRight();
+    change();
+    }
     
-    button="Stop";
+    function moveRight(){
     
-    document.getElementById("Start").disabled = false;
-    document.getElementById("Start").hidden = false;
-    document.getElementById("Stop").disabled = true;
-    document.getElementById("Stop").hidden = true;
-    console.log("Wait...");
-}
+        var r = Math.floor(Math.random() * 256);
+                var g = Math.round(Math.random() * 256);
+                var b = Math.round(Math.random() * 256);
+                var b = Math.round(Math.random() * 256);
+    
+                ColorValue = "rgba("+r+","+g+","+b+",0.1)";
+           
+            
 
-}
+                animate = setTimeout(moveRight,document.getElementById("interval").value*1000);
+                document.body.style.background = ColorValue;
+                
+                
+    }
+    
+    function change(){
+    var elem = document.getElementById("startButton");
+    if (elem.value=="Stop")  elem.value = "Start";
+    else elem.value = "Start";
 
-function Stopcounttext()
-{
-    console.log("Wait...");
-    button="Stop";
-    document.getElementById("Start").disabled = false;
-    document.getElementById("Start").hidden = false;
-    document.getElementById("Stop").disabled = true;
-    document.getElementById("Stop").hidden = true;
-   
-    window.setTimeout( "Stopcounttext()" , document.getElementById("interval").value*1000); // background color change on every 5 sec
- /*
-var index = Math.round(Math.random() * 9);
-var ColorValue = "FFFFFF"; // default color - white (index = 0)
-document.getElementsByTagName("body")[0].style.backgroundColor = ColorValue;
-*/
-}
+    
+    }
+    
+    function stop(){
+        document.getElementById("stopButton").hidden= true;
+        document.getElementById("startButton").hidden = false;
+       
+    clearTimeout(animate);
+
+    }
+    
+    window.onload =init;
