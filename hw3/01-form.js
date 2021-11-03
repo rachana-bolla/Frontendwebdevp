05af37537f5f01d390dba94b7420ecde0ac65104
rachana <bolla.rachana@gmail.com>
2021-11-03 02:52:15 -0700

    form

--- hw3/01-form.js
+++ hw3/01-form.js
@@ -1,3 +1,23 @@
 // Enter your code here
 
-console.log('Enter your code here');
+function submitdata()
+{
+    const myform=document.getElementById("Myform");
+    myform.addEventListener("submit",(e) => {e.preventDefault();console.log("Form has been submitted");})
+    var name = document.getElementById("Name").value;
+   var email = document.getElementById("Email").value;
+  
+   var reg = document.getElementById("registration").value;
+  
+  var clsopt = document.getElementById("classsel").value;
+  var courses = document.getElementById("courses").value;
+  var rawtext = document.getElementById("anyinfo").value;
+    console.group();
+    console.log("Name : "+name);
+    console.log("Email : "+email);
+    console.log("Registration : "+reg);
+    console.log("Class : "+clsopt);
+    console.log("Courses :"+courses);
+    console.log("Anything else? : "+rawtext);
+   
+}
